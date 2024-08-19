326a74beb8da02a3810302fb827d6eab362eb978
Christopher Tessum <ctessum@gmail.com>
2024-08-20 08:19:27 +0900

    Add test and benchmarks

--- bench/runbenchmarks.jl
+++ bench/runbenchmarks.jl
@@ -27,6 +27,55 @@ suite["GEOSFP"] = BenchmarkGroup()
 suite["GEOSFP"]["Interpolation serial"] = @benchmarkable interpfunc_serial()
 suite["GEOSFP"]["Interpolation threaded"] = @benchmarkable interpfunc_threads()
 
+using EarthSciMLBase, ModelingToolkit
+using DomainSets, Dates
+using DifferentialEquations
+using Unitful
+
+struct SysCoupler
+    sys
+end
+function EarthSciMLBase.couple2(sys::SysCoupler, emis::EarthSciData.NEI2016MonthlyEmisCoupler)
+    sys, emis = sys.sys, emis.sys
+    operator_compose(sys, emis)
+end
+function nei_simulator()
+    @parameters t [unit = u"s"]
+    @parameters lat = 0.0 lon = 0.0 lev = 0.0
+    @variables ACET(t) = 0.0 [unit = u"kg*m^-3"]
+    @constants c = 1000 [unit = u"s"]
+    
+    @named sys = ODESystem(
+        [Differential(t)(ACET) ~ 0], t,
+        metadata=Dict(:coupletype => SysCoupler)
+    )
+    
+    emis = NEI2016MonthlyEmis("mrggrid_withbeis_withrwc", t, lon, lat, lev; dtype=Float64)
+    
+    starttime = datetime2unix(DateTime(2016, 5, 1))
+    endtime = datetime2unix(DateTime(2016, 5, 2))
+    domain = DomainInfo(
+        constIC(16.0, t ∈ Interval(starttime, endtime)),
+        constBC(16.0,
+            lon ∈ Interval(-130.0, -60.0),
+            lat ∈ Interval(9.75, 60.0),
+            lev ∈ Interval(1, 2)
+        ))
+    
+    csys = couple(sys, emis, domain)
+    
+    Simulator(csys, [2.0, 2.0, 1])    
+end
+
+suite["NEI Simulator"] = BenchmarkGroup()
+sim = nei_simulator()
+st = SimulatorStrangSerial(Tsit5(), Euler(), 100.0)
+suite["NEI Simulator"]["Serial"] = @benchmarkable run!($sim, $st)
+
+st = SimulatorStrangThreads(Tsit5(), Euler(), 100.0)
+suite["NEI Simulator"]["Threads"] = @benchmarkable run!($sim, $st)
+
+
 tune!(suite, verbose = true)
 results = run(suite, verbose = true)
 
---
+++ test/update_callback_test.jl
@@ -0,0 +1,52 @@
+using Main.EarthSciData
+
+using Test
+using EarthSciMLBase, ModelingToolkit
+using DomainSets, Dates
+using DifferentialEquations
+using Unitful
+
+@parameters t [unit = u"s"]
+@parameters lat = 0.0 lon = 0.0 lev = 0.0
+@variables ACET(t) = 0.0 [unit = u"kg*m^-3"]
+@constants c = 1000 [unit = u"s"]
+
+struct SysCoupler
+    sys
+end
+@named sys = ODESystem(
+    [Differential(t)(ACET) ~ 0], t,
+    metadata=Dict(:coupletype => SysCoupler)
+)
+function EarthSciMLBase.couple2(sys::SysCoupler, emis::EarthSciData.NEI2016MonthlyEmisCoupler)
+    sys, emis = sys.sys, emis.sys
+    operator_compose(sys, emis)
+end
+
+emis = NEI2016MonthlyEmis("mrggrid_withbeis_withrwc", t, lon, lat, lev; dtype=Float64)
+
+starttime = datetime2unix(DateTime(2016, 5, 1))
+endtime = datetime2unix(DateTime(2016, 5, 2))
+domain = DomainInfo(
+    constIC(16.0, t ∈ Interval(starttime, endtime)),
+    constBC(16.0,
+        lon ∈ Interval(-130.0, -60.0),
+        lat ∈ Interval(9.75, 60.0),
+        lev ∈ Interval(1, 2)
+    ))
+
+csys = couple(sys, emis, domain)
+
+sim = Simulator(csys, [2.0, 2.0, 1])
+
+st = SimulatorStrangSerial(Tsit5(), Euler(), 100.0)
+
+sol = run!(sim, st)
+
+@test sum(sol.u[end]) ≈ 8.42487289526795e-5
+
+st = SimulatorStrangThreads(Tsit5(), Euler(), 100.0)
+
+sol = run!(sim, st)
+
+@test sum(sol.u[end]) ≈ 8.42487289526795e-5
