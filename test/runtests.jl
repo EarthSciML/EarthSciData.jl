339fa17af844fe212321c8c4238f883f3b4dbe98
Christopher Tessum <ctessum@gmail.com>
2025-07-31 20:59:04 -0500

    Update test to test item runner

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,3 @@
-using EarthSciData
-using Test, SafeTestsets
+using TestItemRunner
 
-@testset "EarthSciData.jl" begin
-    @safetestset "load" begin
-        include("load_test.jl")
-    end
-    @safetestset "geosfp" begin
-        include("geosfp_test.jl")
-    end
-    @safetestset "wrf" begin
-        include("wrf_test.jl")
-    end
-    @safetestset "nei2016monthly" begin
-        include("nei2016monthly_test.jl")
-    end
-    @safetestset "NetCDFOutputter" begin
-        include("netcdf_output_test.jl")
-    end
-    @safetestset "Solve" begin
-        include("solve_test.jl")
-    end
-    @safetestset "NCEP-NCAR Reanalysis" begin
-        include("NCEP-NCAR_Reanalysis_test.jl")
-    end
-end
+@run_package_tests
